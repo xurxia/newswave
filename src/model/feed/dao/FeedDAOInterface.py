22e040532f862ddec8e9a45a7b6fdbc53922fc34
Xurxia <ggonzalezs@xruxia.net>
2023-12-18 13:51:05 +0100

    Added abstractmethod to FeedDAOInterface

--- src/model/feed/dao/FeedDAOInterface.py
+++ src/model/feed/dao/FeedDAOInterface.py
@@ -1,18 +1,25 @@
+from abc import ABC, abstractmethod
+
 from src.model.feed.dto.FeedDTO import FeedDTO
 
-class FeedDAOInterface():
+class FeedDAOInterface(ABC):
 
+    @abstractmethod
     def create_feed(self, feed : FeedDTO) -> int:
         pass
 
+    @abstractmethod
     def get_feed(self, id : int) -> FeedDTO:
         pass
 
+    @abstractmethod
     def get_feeds(self) -> list[FeedDTO]:
         pass
     
+    @abstractmethod
     def update_feed(self, feed : FeedDTO) -> None:
         pass
 
+    @abstractmethod
     def delete_feed(self, id : int) -> None:
         pass
