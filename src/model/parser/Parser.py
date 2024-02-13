b703fdf972bbeeecf597f3ac1823ae7d76dd53bf
Xurxia <ggonzalezs@xurxia.net>
2024-02-14 00:11:23 +0100

    Added Exception management

--- src/model/parser/Parser.py
+++ src/model/parser/Parser.py
@@ -2,7 +2,7 @@ from time import mktime
 from datetime import datetime, timedelta
 
 from src.model.common.Config import Config
-from src.model.feed.facade.FeedFacade import FeedFacade, FeedDTO, EntryDTO
+from src.model.feed.facade.FeedFacade import FeedFacade, FeedDTO, EntryDTO, ModelException
 
 class Parser():
 	
@@ -16,14 +16,20 @@ class Parser():
         html = '<!DOCTYPE html><html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"></head><body>'
         feeds : list[FeedDTO] = self._feed_facade.get_feeds()
         for feed in feeds:
-            print(f'Source: {feed.name}')
-            html += f'<H2>Fuente: {feed.name}</H2>'
-            html += "<ul>"
-            entries = self._feed_facade.get_entries(feed)
-            for entry in entries:
-                if entry.published > start_date:
-                    html += f'<li>{str(entry.published)} : <a href="{entry.link}">{entry.title}</a></li>'
-            html += '</ul>'
+            html_tmp = ''
+            try:
+                print(f'Source: {feed.name}')
+                html_tmp += f'<H2>Fuente: {feed.name}</H2>'
+                html_tmp += "<ul>"
+                entries = self._feed_facade.get_entries(feed)
+                for entry in entries:
+                    if entry.published > start_date:
+                        html_tmp += f'<li>{str(entry.published)} : <a href="{entry.link}">{entry.title}</a></li>'
+                html_tmp += '</ul>'
+            except ModelException as e:
+                print(f'Error: {e.message}')
+            else:
+                html += html_tmp
         html += "</body></html>"
         with open(self._file, "w", encoding="utf-8") as output:
             output.write(html)
