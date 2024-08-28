32bf16540c90cb0c94525a69fbd5d599ce9383da
Xurxia <ggonzalezs@xurxia.net>
2024-08-28 21:59:27 +0200

    Modified Config class to add set and write methods

--- src/model/common/Config.py
+++ src/model/common/Config.py
@@ -2,9 +2,11 @@ from configparser import ConfigParser, NoSectionError, NoOptionError
 
 from src.model.exception.ModelException import ModelException
 
+DEFAULT_FILE = './src/config/config.ini'
+
 class Config():
 
-    def __new__(cls, file = './src/config/config.ini'):
+    def __new__(cls, file = DEFAULT_FILE):
         if not hasattr(cls, '_singleton'):
             cls._singleton = super(Config, cls).__new__(cls)
         return cls._singleton
@@ -17,32 +19,66 @@ class Config():
             except Exception as e:
                 raise ModelException(f'Error reading config file: {file}')
     
-    def get_str(self, section : str, parameter : str) -> str | None:
+    def _get(self, section : str, parameter : str) -> str | int | float | None:
         try:
             return self._config.get(section, parameter)
         except NoSectionError as e:
+            raise ModelException(f'Error setting section {section}')
+        except NoOptionError as e:
+            raise ModelException(f'Error setting parameter {parameter} in section {section}')
+        except Exception as e:
+            raise ModelException('Error setting a parameter in config file')
+
+    def _set(self, section: str, parameter : str, value : float) -> None:
+        try:
+            self._config.set(section, parameter, value)
+        except NoSectionError as e:
             raise ModelException(f'Error getting section {section}')
         except NoOptionError as e:
             raise ModelException(f'Error getting parameter {parameter} from section {section}')
         except Exception as e:
             raise ModelException('Error accesing a parameter from config file')
-    
+
+
+    def get_str(self, section : str, parameter : str) -> str | None:
+        try:
+            return self._get(section, parameter)
+        except Exception as e:
+            raise e
+        
+    def set_str(self, section: str, parameter : str, value : str) -> None:
+        try:
+            return self._set(section, parameter, value)
+        except Exception as e:
+            raise e
+        
     def get_int(self, section : str, parameter : str) -> int | None:
         try:
-            return self._config.getint(section, parameter)
-        except NoSectionError as e:
-            raise ModelException(f'Error getting section {section}')
-        except NoOptionError as e:
-            raise ModelException(f'Error getting parameter {parameter} from section {section}')
+            return self._get(section, parameter)
         except Exception as e:
-            raise ModelException('Error accesing a parameter from config file')
-    
+            raise e
+            
+    def set_int(self, section: str, parameter : str, value : int) -> None:
+        try:
+            return self._set(section, parameter, value)
+        except Exception as e:
+            raise e
+        
     def get_float(self, section : str, parameter : str) -> float | None:
         try:
-            return self._config.getfloat(section, parameter)
-        except NoSectionError as e:
-            raise ModelException(f'Error getting section {section}')
-        except NoOptionError as e:
-            raise ModelException(f'Error getting parameter {parameter} from section {section}')
+            return self._get(section, parameter)
         except Exception as e:
-            raise ModelException('Error accesing a parameter from config file')
+            raise e
+        
+    def set_flotat(self, section: str, parameter : str, value : float) -> None:
+        try:
+            return self._set(section, parameter, value)
+        except Exception as e:
+            raise e
+        
+    def write(self, file = DEFAULT_FILE) -> None:
+        try:
+            with open(file, 'w') as fp:
+                self._config.write(fp)
+        except Exception as e:
+            raise ModelException(f'Error writing config file: {file}')
