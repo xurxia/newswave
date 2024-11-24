71d77ad78d647f234c551c64c1f28aff956700fe
Xurxia <ggonzalezs@xurxia.net>
2024-11-24 20:04:41 +0100

    Added config file as command line parameter

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ from src.model.parser.Parser import Parser
 from src.model.email.Email import Email
 from src.model.common.Config import Config
 
+import sys
+
 if __name__ == '__main__':
+    config_file : str = './src/config/config.ini'
+    if len(sys.argv)==2:
+        config_file = sys.argv[1]
+        
+    config : Config = Config(file=config_file)
     parser : Parser = Parser()
     email : Email = Email()
     
